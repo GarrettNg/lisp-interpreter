ef7e25d23635add6cf1319fe600095bca60056c9
Garrett Ng <email@email.com>
2020-06-24 18:19:29 -0400

    Initial commit

---
+++ lis.py
@@ -0,0 +1,147 @@
+'''
+Lisp Interpreter (Scheme) in Python
+Adapted from https://www.norvig.com/lispy.html
+(Peter  Norvig, (c) 2010-16)
+'''
+
+import math
+import operator as op
+
+### Type Definitions
+Symbol = str
+Number = (int, float)
+Atom = (Symbol, Number)
+List = list
+Exp = (Atom, List)
+Env = dict
+
+### Parsing
+def tokenize(chars: str) -> list:
+    "Convert a string of characters into a list of tokens."
+    return chars.replace('(', ' ( ').replace(')', ' ) ').split()
+
+def parse(program: str) -> Exp:
+    "Read a Scheme expression from a string."
+    return read_from_tokens(tokenize(program))
+
+def read_from_tokens(tokens: list) -> Exp:
+    "Read an expression from a sequence of tokens."
+    if len(tokens) == 0:
+        raise SyntaxError('unexpected EOF')
+    token = tokens.pop(0)
+    if token == '(':
+        L = []
+        while tokens[0] != ')':
+            L.append(read_from_tokens(tokens))
+        tokens.pop(0) # pop off ')'
+        return L
+    elif token == ')':
+        raise SyntaxError('unexpected )')
+    else:
+        return atom(token)
+
+def atom(token: str) -> Atom:
+    "Numbers become numbers; every other token is a symbol."
+    try: return int(token)
+    except ValueError:
+        try: return float(token)
+        except ValueError:
+            return Symbol(token)
+
+### Environments
+def standard_env() -> Env:
+    "An environment with some Scheme standard procedures."
+    env = Env()
+    env.update(vars(math)) # sin, cos, sqrt, pi, ...
+    env.update({
+        '+':op.add, '-':op.sub, '*':op.mul, '/':op.truediv,
+        '>':op.gt, '<':op.lt, '>=':op.ge, '<=':op.le, '=':op.eq,
+        'abs':          abs,
+        'append':       op.add,
+        'apply':        lambda proc, args: proc(*args),
+        'begin':        lambda *x: x[-1],
+        'car':          lambda x: x[0],
+        'cdr':          lambda x: x[1:],
+        'cons':         lambda x,y: [x] + y,
+        'eq?':          op.is_,
+        'expt':         pow,
+        'equal?':       op.eq,
+        'length':       len,
+        'list':         lambda *x: List(x),
+        'list?':        lambda x: isinstance(x, List),
+        'map':          map,
+        'max':          max,
+        'min':          min,
+        'not':          op.not_,
+        'null?':        lambda x: x == [],
+        'number?':      lambda x: isinstance(x, Number),
+        'print':        print,
+        'procedure?':   callable,
+        'round':        round,
+        'symbol?':      lambda x: isinstance(x, Symbol),
+        })
+    return env
+
+class Env(dict):
+    "An environment: a dict of {'var': val} pairs, with an outer Env."
+    def __init__(self, params=(), args=(), outer=None):
+        self.update(zip(params, args))
+        self.outer = outer
+    def find(self, var):
+        "Find the innermost Env where var appears."
+        return self if (var in self) else self.outer.find(var)
+
+global_env = standard_env()
+
+### Evaluation
+def eval(x: Exp, env=global_env) -> Exp:
+    "Evaluate an expression in an environment."
+    if isinstance(x, Symbol):       # variable reference
+        return env.find(x)[x]
+    elif not isinstance(x, List):   # constant
+        return x
+    op, *args = x
+    if op == 'quote':               # quotation
+        return args[0]
+    elif op == 'if':                # conditional
+        (test, conseq, alt) = args
+        exp = (conseq if eval(test, env) else alt)
+        return eval(exp, env)
+    elif op == 'define':            # definition
+        (symbol, exp) = args
+        env[symbol] = eval(exp, env)
+    elif op == 'set!':              # assignment
+        (symbol, exp) = args
+        env.find(symbol)[symbol] = eval(exp, env)
+    elif op == 'lambda':            # procedure
+        (params, body) = args
+        return Procedure(params, body, env)
+    else:                           # procedure call
+        proc = eval(op, env)
+        vals = [eval(arg, env) for arg in args]
+        return proc(*vals)
+
+### Procedures
+class Procedure(object):
+    "A user-defined Scheme procedure."
+    def __init__(self, params, body, env):
+        self.params, self.body, self.env = params, body, env
+    def __call__(self, *args):
+        return eval(self.body, Env(self.params, args, self.env))
+
+### REPL
+def repl(prompt='lis.py> '):
+    "A prompt-read-eval-print loop."
+    while True:
+        val = eval(parse(input(prompt)))
+        if val is not None:
+            print(schemestr(val))
+
+def schemestr(exp):
+    "Convert a Python object back into a Scheme-readable string."
+    if isinstance(exp, List):
+        return '(' + ' '.join(map(schemestr, exp)) + ')'
+    else:
+        return str(exp)
+
+repl()
